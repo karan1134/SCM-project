fe81446cb3dd58242732abd7beb8c86246cfc90d
karan1134 <46700802+karan1134@users.noreply.github.com>
2019-04-22 13:00:04 +0530

    function created for sorting an array

--- project.cpp
+++ project.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
-using namespace std;
+using namespace std; 
+int sort(){                                                                       //this is program to sort an array.
+
+	 int numbers[5], i ,j ,temp;
+
+	cout<<"Simple C++ Example Program for Sorting (Asending Order) In Array\n";
+
+	// Read Input
+    for (i = 0; i < 5; i++)
+    {
+		cout<<"Enter the Number : "<< (i+1) <<"  : ";
+        cin>>numbers[i];
+    }
+        
+    // Array Sorting - Asensding Order
+    for (i = 0; i < 5; ++i)
+    {
+        for (j = i + 1; j < 5; ++j)
+        {
+            if (numbers[i] > numbers[j])
+            {
+                temp =  numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+    }
+    
+    cout<<"Sorting Order Array: \n";
+    for (i = 0; i < 5; ++i)
+        cout<<numbers[i]<<endl;
+}
 
 
 int biggestno(){                                                             //Gives the biggest number in an array.
@@ -34,12 +65,14 @@ int main()
              int option=-1;
              while(option)
                {
-                 printf("1.Largest number in array. 2.xyz 3.Exit\n");
+                 printf("1.Largest number in array. 2.Sorting an array 3.Exit\n");
                  printf("Enter your choice :- ");
                  scanf("%d",&choice);
                  switch(choice)
                  {
                    case 1: biggestno() ;
+                           break;
+                    case 2: sort();
                            break;
                    
                  }
