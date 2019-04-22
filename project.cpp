4ef3c53c9feb0ea3f76f66d61f63ce040f5d141a
karan1134 <46700802+karan1134@users.noreply.github.com>
2019-04-22 13:26:01 +0530

    matrix addition

--- project.cpp
+++ project.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 
 using namespace std; 
+int madd(){                                                                                           //matrix addition.
+int rowCount, columnCount, i, j;
+    int firstMatrix[10][10], secondMatrix[10][10], resultMatrix[10][10];                                         
+
+	cout<<"Simple C++ Example Program for 2 D (dimensional) Array Matrix Addition Example\n";
+
+    cout<<"Number of rows of matrices to be added : ";
+    cin>>rowCount;
+
+    cout<<"Number of columns matrices to be added : ";
+    cin>>columnCount;
+
+    cout<<"Elements of first matrix : \n";
+
+    for (i = 0; i < rowCount; i++)
+        for (j = 0; j < columnCount; j++)
+            cin>>firstMatrix[i][j];
+
+    cout<<"Elements of second matrix : \n";
+
+    for (i = 0; i < rowCount; i++)
+        for (j = 0; j < columnCount; j++)
+            cin>>secondMatrix[i][j];
+
+    cout<<"Sum of entered matrices : \n";
+
+    for (i = 0; i < rowCount; i++)
+    {
+        for (j = 0; j < columnCount; j++)
+        {
+            resultMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+            cout<<resultMatrix[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
+    
+    
+
+    return 0;
+}
+
+
+/*....................................................*/
+
+
+
 int search(){                                                                 //this is a program to search a number in an array
 	 int numbers[5], i ,search_key;
 
@@ -99,7 +145,7 @@ int main()
              int option=-1;
              while(option)
                {
-                 printf("1.Largest number in array. 2.Sorting an array 3.Searching a number in aaray 4.Exit\n");
+                 printf("1.Largest number in array. 2.Sorting an array 3.Searching a number in aaray, 4.Matrix addition, 5.Exit\n");
                  printf("Enter your choice :- ");
                  scanf("%d",&choice);
                  switch(choice)
@@ -110,7 +156,8 @@ int main()
                            break;
                    case 3: search();
                            break;
-                           
+                   case 4: madd();
+				           break;
                    
                  }
                  fflush(stdin);
